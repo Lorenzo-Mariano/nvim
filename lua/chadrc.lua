15f183130427d4316fcb0df96bebff47dcee1528
Lorenzo-Mariano <hhumminah@gmail.com>
2024-11-06 12:04:37 +0800

    Change floating terminal size

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,30 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
-M.base46 = {
-	theme = "onedark",
+M.term = {
+  float = {
+    relative = "editor",
+    row = 0.125,
+    col = 0.275,
+    height = 0.75,
+    width = 0.45,
+  },
+}
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+M.nvdash = {
+  load_on_startup = true,
+}
+
+M.base46 = {
+  theme = "bearded-arc",
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 return M

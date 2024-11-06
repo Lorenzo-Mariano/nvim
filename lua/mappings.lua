a5a3d034980ef11db67787e8cd0377013fc97ec1
Lorenzo-Mariano <hhumminah@gmail.com>
2024-11-06 12:05:46 +0800

    Remove unused mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,14 +1,9 @@
 require "nvchad.mappings"
-local cmp = require "cmp"
 
 -- add yours here
 
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-map("i", "<C-Space>", function()
-  cmp.mapping.complete()
-end, { desc = "Trigger completion" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")

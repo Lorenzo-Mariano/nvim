6edc7d607107a806bd9f8177faa289081316e6a3
Lorenzo-Mariano <hhumminah@gmail.com>
2024-11-06 12:05:22 +0800

    Use relative numbers by default

--- lua/options.lua
+++ lua/options.lua
@@ -7,6 +7,7 @@ require "nvchad.options"
 -- vim.opt.shell = "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
 
 vim.opt.shell = "C:/Users/loren/AppData/Local/Programs/nu/bin/nu.exe"
+vim.opt.relativenumber = true
 
 -- vim.opt.shell = "C:/Users/loren/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Windows PowerShell/Windows PowerShell"
 

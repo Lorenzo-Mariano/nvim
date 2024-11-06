8a5ef20add8f548f2e7d47f7698c85e2b9ac3b82
Lorenzo-Mariano <hhumminah@gmail.com>
2024-11-06 12:21:02 +0800

    Remove cmp from plugins/init.lua as it's unneeded

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,18 +17,6 @@ return {
     end,
   },
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-    },
-    config = function()
-      require "configs.cmp"
-    end,
-  },
-  {
     "roobert/tailwindcss-colorizer-cmp.nvim",
     config = function()
       require("tailwindcss-colorizer-cmp").setup()

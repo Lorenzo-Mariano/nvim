d2ea7b3154c90e68ae5e8b45dca1adc0c2277f6b
Lorenzo-Mariano <hhumminah@gmail.com>
2024-11-23 20:02:07 +0800

    Alt + \ now opens a floating terminal instead of Alt + i

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,5 +5,8 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+map({ "n", "t" }, "<A-\\>", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "terminal toggle floating term" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/options.lua
+++ lua/options.lua
@@ -3,8 +3,8 @@ require "nvchad.options"
 -- add yours here!
 
 -- vim.opt.shell = "C:\\cygwin64\\bin\\bash.exe"
--- vim.opt.shell = "C:/Program Files/PowerShell/7/pwsh.exe"
-vim.opt.shell = "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
+vim.opt.shell = "C:/Program Files/PowerShell/7/pwsh.exe"
+-- vim.opt.shell = "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
 
 -- vim.opt.shell = "C:/Users/loren/AppData/Local/Programs/nu/bin/nu.exe"
 vim.opt.relativenumber = true

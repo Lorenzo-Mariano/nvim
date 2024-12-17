8c12c149dd793d31d50a540886ae9514c68b6f2c
Lorenzo-Mariano <hhumminah@gmail.com>
2024-12-17 12:53:28 +0800

    Add powershell as shell, enable relative numbers, add mapping Alt + \ to open floating terminal

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,8 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map({ "n", "t" }, "<A-\\>", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "terminal toggle floating term" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/options.lua
+++ lua/options.lua
@@ -5,4 +5,5 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
+vim.opt.shell = "C:/Program Files/WindowsApps/Microsoft.PowerShell_7.4.6.0_x64__8wekyb3d8bbwe/pwsh.exe"
 vim.opt.relativenumber = true

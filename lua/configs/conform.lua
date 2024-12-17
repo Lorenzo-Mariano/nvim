45b87897897cc5ec7c906d2055056ac1e8db9d5e
Lorenzo-Mariano <hhumminah@gmail.com>
2024-12-17 14:00:32 +0800

    Add formatters per filetype

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,12 +1,21 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    js = { "prettier" },
+    javascriptreact = { "prettier" },
+    jsx = { "prettier" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
+    tsx = { "prettier" },
+    json = { "prettier" },
+    php = { "intelephense" },
+    yaml = { "prettier" },
   },
 
   format_on_save = {
-  --   -- These options will be passed to conform.format()
+    --   -- These options will be passed to conform.format()
     timeout_ms = 500,
     lsp_fallback = true,
   },

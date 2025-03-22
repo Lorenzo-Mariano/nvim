ec4febe8a15ee5e1b503b80585808e524b02a8a0
Lorenzo Mariano <hhumminah@gmail.com>
2025-03-22 16:26:04 +0800

    Add lazy package manager, neotree, treesitter, noice and transparent

--- mappings.lua
+++ mappings.lua
@@ -1,11 +1,45 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+local builtin = require('telescope.builtin')
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
+-- make ; open command mode
 map('n', ';', ':', { noremap = true })
 
 -- Alt + S to save file
 map('n', '<M-s>', ':w<CR>', opts)
 
+-- Use "p" to paste from the system clipboard
+vim.api.nvim_set_keymap('n', 'p', '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'P', '"+P', { noremap = true, silent = true })
+
+-- Use "y" to copy to the system clipboard
+vim.api.nvim_set_keymap('v', 'y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'yy', '"+yy', { noremap = true, silent = true })
+
+-- Use spaces instead of tabs
+vim.opt.expandtab = true  
+
+-- Set tab width to 4 spaces
+vim.opt.shiftwidth = 4  
+vim.opt.tabstop = 4  
+
+-- When pressing <Tab> in insert mode, insert spaces instead of a tab character
+vim.opt.softtabstop = 4  
+
+-- Telescope stuff
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- Move focus
+map('n', '<C-h>', '<C-w>h', opts) 
+map('n', '<C-l>', '<C-w>l', opts)
+map('n', '<C-k>', '<C-w>k', opts)
+map('n', '<C-j>', '<C-w>j', opts)
+
+-- Toggle Neotree
+map('n', '<C-b>', ':Neotree toggle<CR>', opts)
---
+++ plugins.lua
@@ -0,0 +1,54 @@
+return {
+{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    cmd = "Telescope",
+    lazy = false,
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+{
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  lazy = false,
+  opts = {
+  },
+},
+   {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        signature = {
+          enabled = false, -- to stop it from covering my whole face when typing
+        },
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = true,
+      },
+    },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+  },
+{
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    config = function()
+      require("transparent")
+    end,
+  },
+}

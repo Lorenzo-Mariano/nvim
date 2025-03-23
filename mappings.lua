ac82111e9efde80d3a62343d7e14e91a5e4777fa
Lorenzo Mariano <hhumminah@gmail.com>
2025-03-23 11:40:25 +0800

    Bind leader + fw to live grep

--- mappings.lua
+++ mappings.lua
@@ -31,7 +31,7 @@ vim.opt.softtabstop = 4
 
 -- Telescope stuff
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 

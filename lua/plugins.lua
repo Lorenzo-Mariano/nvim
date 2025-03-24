b4e02fe968c32bfb252fcaa77b8067afa759a3a6
Lorenzo Mariano <hhumminah@gmail.com>
2025-03-24 15:12:32 +0800

    Remove highlight background for git merge text

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -63,7 +63,22 @@ return {
 	{
 		"akinsho/git-conflict.nvim",
 		version = "*",
-		config = true,
+		config = function()
+			-- Lighter highlights so my corneas stay intact before I turn 21
+			vim.api.nvim_set_hl(0, "GitConflictCurrent", { fg = "#7aa2f7", bg = "NONE", bold = true })
+			vim.api.nvim_set_hl(0, "GitConflictIncoming", { fg = "#9ece6a", bg = "NONE", bold = true })
+
+			require("git-conflict").setup({
+				default_mappings = true,
+				default_commands = true,
+				disable_diagnostics = false,
+				list_opener = "copen",
+				highlights = {
+					incoming = "GitConflictIncoming",
+					current = "GitConflictCurrent",
+				},
+			})
+		end,
 	},
 	{
 		"nvim-telescope/telescope.nvim",

e765a3a127e7709805f3b4d0016d3cd691bd5b8c
Lorenzo Mariano <hhumminah@gmail.com>
2025-03-25 08:50:45 +0800

    Make blink show docs, Ctrl + l to show list

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -34,15 +34,41 @@ return {
 		dependencies = { "rafamadriz/friendly-snippets" },
 		version = "*",
 		opts = {
-			keymap = { preset = "default" },
+			keymap = {
+				["<C-u>"] = {
+					function(cmp)
+						cmp.scroll_documentation_up(4)
+					end,
+				},
+
+				["<C-d>"] = {
+					function(cmp)
+						cmp.scroll_documentation_down(4)
+					end,
+				},
+
+				["<C-l>"] = {
+					function(cmp)
+						cmp.show()
+					end,
+				},
+			},
 			appearance = {
 				nerd_font_variant = "mono",
 			},
+
 			sources = {
 				default = { "lsp", "path", "snippets", "buffer" },
 			},
 
 			fuzzy = { implementation = "prefer_rust_with_warning" },
+
+			completion = {
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 100,
+				},
+			},
 		},
 		opts_extend = { "sources.default" },
 	},

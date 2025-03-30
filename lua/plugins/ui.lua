280a36fa26fa519442a259b54d0c5f6a9a1f5b5f
Lorenzo Mariano <hhumminah@gmail.com>
2025-03-30 17:10:19 +0800

    Add smear-cursor, switch back to manual lspconfig

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,5 +1,13 @@
 return {
 	{
+		"sphamba/smear-cursor.nvim",
+		opts = {
+			stiffness = 0.8,
+			trailing_stiffness = 0.5,
+			distance_stop_animating = 0.1,
+		},
+	},
+	{
 		"xiyaowong/transparent.nvim",
 		lazy = false,
 		config = function()
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -6,25 +6,66 @@ return {
 		end,
 	},
 	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = { "neovim/nvim-lspconfig" },
+		"neovim/nvim-lspconfig",
 		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls" },
-				automatic_installation = true,
+			local lspconfig = require("lspconfig")
+			local servers = { "ts_ls", "yamlls" }
+			for _, lsp in ipairs(servers) do
+				lspconfig[lsp].setup({})
+			end
+
+			lspconfig.omnisharp.setup({
+				cmd = {
+					"dotnet",
+					vim.fn.expand(
+						"C:/Users/Nyormensho/AppData/Local/nvim-data/mason/packages/omnisharp/libexec/OmniSharp.dll"
+					),
+				},
+				root_dir = lspconfig.util.root_pattern("*.sln", ".git"),
+				settings = {
+					RoslynExtensionsOptions = { EnableImportCompletion = true },
+					MsBuild = { LoadProjectsOnDemand = false },
+				},
 			})
-			require("mason-lspconfig").setup_handlers({
-				function(server_name)
-					require("lspconfig")[server_name].setup({})
-				end,
-				["lua_ls"] = function()
-					require("lspconfig").lua_ls.setup({
-						settings = {
-							Lua = {
-								diagnostics = { globals = { "vim" } },
-							},
-						},
-					})
+		end,
+	},
+	-- {
+	-- 	"williamboman/mason-lspconfig.nvim",
+	-- 	dependencies = { "neovim/nvim-lspconfig" },
+	-- 	config = function()
+	-- 		require("mason-lspconfig").setup({
+	-- 			ensure_installed = { "lua_ls" },
+	-- 			automatic_installation = true,
+	-- 		})
+	-- 		require("mason-lspconfig").setup_handlers({
+	-- 			function(server_name)
+	-- 				require("lspconfig")[server_name].setup({})
+	-- 			end,
+	-- 			["lua_ls"] = function()
+	-- 				require("lspconfig").lua_ls.setup({
+	-- 					settings = {
+	-- 						Lua = {
+	-- 							diagnostics = { globals = { "vim" } },
+	-- 						},
+	-- 					},
+	-- 				})
+	-- 			end,
+	-- 		})
+	-- 	end,
+	-- },
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			require("lint").linters_by_ft = {
+				javascript = { "eslint_d" },
+				typescript = { "eslint_d" },
+				javascriptreact = { "eslint_d" },
+				typescriptreact = { "eslint_d" },
+			}
+
+			vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+				callback = function()
+					require("lint").try_lint()
 				end,
 			})
 		end,
